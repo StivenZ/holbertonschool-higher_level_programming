5dec2e8dd7d33dfc875f476bf7b33343e6096987
Stiven Espinosa <1806@holbertonschool.com>
2020-09-04 06:34:52 +0000

    Correction variable declaration

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import add_0 as ad
-    ad.a = 1
-    ad.b = 2
-    print("{} + {} = {}".format(ad.a, ad.b, ad.add(1, 2)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, ad.add(a, b)))
