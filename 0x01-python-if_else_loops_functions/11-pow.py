3113838a004ff08ecc0acac8452ec0b052607863
stivenz <1806@holbertonschool.com>
2020-09-06 12:08:57 -0500

    Returns the power of a number

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def pow(a, b):
+    power = 1
+    if b == 0:
+        return(power)
+    elif b > 0:
+        for i in range(b):
+            power = power * a
+        return(power)
+    elif b < 0:
+        for i in range(abs(b)):
+            power = (1.0/a) * power
+        return(power)
