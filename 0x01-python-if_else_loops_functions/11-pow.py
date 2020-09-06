3245a9bef38b58112805ee2f2a746f37c616eb92
stivenz <1806@holbertonschool.com>
2020-09-06 12:19:06 -0500

    Fixed range

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 def pow(a, b):
     power = 1
+    i = 0
     if b == 0:
         return(power)
     elif b > 0:
-        for i in range(b):
+        while i < b:
             power = power * a
+            i += 1
         return(power)
     elif b < 0:
-        for i in range(abs(b)):
+        while i < abs(b):
             power = (1/a) * power
+            i += 1
         return(power)
