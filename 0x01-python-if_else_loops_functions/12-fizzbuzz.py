a79d5d6ffc4cc93fd61678d1005ba91bc8645094
stivenz <1806@holbertonschool.com>
2020-09-06 19:05:48 -0500

    Fixed last item

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for a in range(1, 101):
-        if a != range(1, 101)[-1]:
-            if a % 5 == 0 and a % 3 == 0:
-                print("FizzBuzz ", end='')
-                continue
-            elif a % 3 == 0:
-                print("Fizz ", end='')
-                continue
-            elif a % 5 == 0:
-                print("Buzz ", end='')
-                continue
-            else:
-                print(a, end=' ')
+        if a % 5 == 0 and a % 3 == 0:
+            print("FizzBuzz ", end='')
+            continue
+        elif a % 3 == 0:
+            print("Fizz ", end='')
+            continue
+        elif a % 5 == 0:
+            print("Buzz ", end='')
+            continue
+        else:
+            print(a, end=' ')
