b6a67294c999818038750f0af83c48563f7ad988
stivenz <1806@holbertonschool.com>
2020-09-06 19:01:32 -0500

    Fixed missing whitespace

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,14 +2,14 @@
 def fizzbuzz():
     for a in range(1, 101):
         if a != range(1, 101)[-1]:
-            if a % 3 == 0:
+            if a % 5 == 0 and a % 3 == 0:
+                print("FizzBuzz ", end='')
+                continue
+            elif a % 3 == 0:
                 print("Fizz ", end='')
                 continue
             elif a % 5 == 0:
                 print("Buzz ", end='')
-                continue
-            elif a % 5 == 0 and a % 3 == 0:
-                print("FizzBuzz ", end='')
                 continue
             else:
                 print(a, end=' ')
