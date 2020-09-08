4743eceaeb23cd33a9931b624322df5cb084494f
stivenz <1806@holbertonschool.com>
2020-09-08 17:29:52 -0500

    Givin permissions and checking list existence

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
-        return(my_list[:])
-    new = my_list.copy()
-    new[idx] = element
-    return(new)
+    if my_list:
+        if idx < 0 or idx > len(my_list) - 1:
+            return(my_list[:])
+        new = my_list.copy()
+        new[idx] = element
+        return(new)
