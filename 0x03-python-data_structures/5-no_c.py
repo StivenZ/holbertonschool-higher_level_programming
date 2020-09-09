d0aff102adf6016c78aa208ed9e4d4af956f9057
stivenz <1806@holbertonschool.com>
2020-09-09 02:20:02 -0500

    Fixing index misspositioning

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    # Struggled trying to find out why the uncommented
+    # solution wouldnt work in some cases: we must not
+    # change the list in which we are iterating:
+    #  back up and iterate over the original, otherwise we may be
+    # struggling with index misspositioning.
+
+    # newstr = ""
+    # for char in my_string:
+    #     if char != 'C' and char != 'c':
+    #         mystr += char
+    # return newstr
+
     if my_string:
         string_list = list(my_string)
-        for i in string_list:
+        for i in list(my_string):
             if i == 'c' or i == 'C':
                 string_list.remove(i)
-            else:
-                continue
         new_string = "".join(string_list)
     return(new_string)
