404637f2aa465cb37f53a0fb5f0e667590ef5bfd
stivenz <1806@holbertonschool.com>
2020-09-09 18:08:35 -0500

    Fixed null case

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
+    if len(matrix) == 1 and not matrix[0]:
+        print("")
+    elif matrix:
         for a in matrix:
             for idx, l in enumerate(a):
                 if idx != len(a) - 1:
