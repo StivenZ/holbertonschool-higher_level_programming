3ef9c4ad15c9dc9fbf54f81e7c1d828b5ffdf76b
stivenz <1806@holbertonschool.com>
2020-09-11 10:00:39 -0500

    Fixed style

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,8 @@
 def roman_to_int(roman_string):
     if isinstance(roman_string, str):
 
-        ro_di = {
-            'I': 1, 'V': 5, 'X': 10, 'C': 100, 'L': 50,
-            'D': 500, 'M': 1000
-            }
+        ro_di = {'I': 1, 'V': 5, 'X': 10, 'C': 100, 'L': 50, \
+                'D': 500, 'M': 1000}
         num_list = list(roman_string)
         add_list = []
         result = 0
@@ -13,14 +11,10 @@ def roman_to_int(roman_string):
         for idx, i in enumerate(num_list):
             if len(num_list) == 1:
                 return(ro_di[i])
-            if (idx + 1 <= len(num_list) - 1) and
-            (ro_di[i]) >= (ro_di[num_list[idx + 1]]):
-
+            if (idx + 1 <= len(num_list) - 1) and (ro_di[i]) >= (ro_di[num_list[idx + 1]]):
                 add_list.append(ro_di[i])
                 continue
-            elif (idx + 1 <= len(num_list) - 1) and
-            (ro_di[i] < ro_di[num_list[idx + 1]]):
-
+            elif (idx + 1 <= len(num_list) - 1) and (ro_di[i] < ro_di[num_list[idx + 1]]):
                 add_list.append(ro_di[num_list[idx + 1]] - ro_di[i])
                 del num_list[idx]
                 if idx + 1 <= len(num_list) - 1:
