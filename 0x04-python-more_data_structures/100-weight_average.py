254900562765f69894252ec89a03ac221e82c26b
stivenz <1806@holbertonschool.com>
2020-09-11 17:17:17 -0500

    Asgard

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if my_list:
+        sum_pond = sum(map(lambda x: x[0] * x[1], my_list))
+        den_sum = sum(map(lambda x: x[1], my_list))
 
-    sum_pond = sum(map(lambda x: x[0] * x[1], my_list))
-    den_sum = sum(map(lambda x: x[1], my_list))
-
-    return(sum_pond / den_sum)
+        return(sum_pond / den_sum)
+    else:
+        return(0)
