3e59a042e2339ac6dcad80e73a03c50c619a0f35
stivenz <1806@holbertonschool.com>
2020-09-11 03:51:24 -0500

    Converts to roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    ro_di = {'I': 1, 'V': 5, 'X': 10, 'C': 100, 'L': 50, \
-            'D': 500, 'M': 1000}
-    num_list = []
-    for i in roman_string:
-        num_list.append(i)
-    add_list = []
-    result = 0
+    if isinstance(roman_string, str):
 
-    for idx, i in enumerate(num_list):
-        if (ro_di[num_list[idx + 1]]) and (ro_di[i]) >= (ro_di[num_list[idx + 1]]):
+        ro_di = {'I': 1, 'V': 5, 'X': 10, 'C': 100, 'L': 50, \
+                'D': 500, 'M': 1000}
+        num_list = list(roman_string)
+        add_list = []
+        result = 0
+
+        for idx, i in enumerate(num_list):
+            # print("idx is {} and item is {} and length is {:d}".format(idx, i, len(num_list)))
+            if len(num_list) == 1:
+                return(ro_di[i])
+            if (idx + 1 <= len(num_list) - 1) and (ro_di[i]) >= (ro_di[num_list[idx + 1]]):
+                add_list.append(ro_di[i])
+                # print("The current sum is {}".format(add_list))
+                continue
+            elif (idx + 1 <= len(num_list) - 1) and (ro_di[i] < ro_di[num_list[idx + 1]]):
+                add_list.append(ro_di[num_list[idx + 1]] - ro_di[i])
+                del num_list[idx]
+                if idx + 1 <= len(num_list) - 1:
+                    continue
+                else:
+                    break
             add_list.append(ro_di[i])
-            continue
-        elif ro_di[i] <= ro_di[num_list[idx + 1]]:
-            add_list.append(ro_di[num_list[idx + 1]] - ro_di[i])
-            num_list.remove(i)
-            continue
-
         for i in add_list:
             result = result + i
         return(result)
+    else:
+        return(0)
