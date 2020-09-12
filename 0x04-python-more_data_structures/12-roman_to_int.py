b19549fdd48a36ea6531843f07d6da844aadf605
stivenz <1806@holbertonschool.com>
2020-09-11 19:33:54 -0500

    Fixed style

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if isinstance(roman_string, str):
-
-        ro_di = {'I': 1, 'V': 5, 'X': 10, 'C': 100, 'L': 50, \
-                'D': 500, 'M': 1000}
-        num_list = list(roman_string)
-        add_list = []
+        rom = {'I': 1, 'V': 5, 'X': 10, 'C': 100, 'L': 50, 'D': 500, 'M': 1000}
+        num = list(roman_string)
+        add = []
         result = 0
-
-        for idx, i in enumerate(num_list):
-            if len(num_list) == 1:
-                return(ro_di[i])
-            if (idx + 1 <= len(num_list) - 1) and (ro_di[i]) >= (ro_di[num_list[idx + 1]]):
-                add_list.append(ro_di[i])
+        for idx, i in enumerate(num):
+            if len(num) == 1:
+                return(rom[i])
+            if (idx + 1 <= len(num) - 1) and (rom[i]) >= (rom[num[idx + 1]]):
+                add.append(rom[i])
                 continue
-            elif (idx + 1 <= len(num_list) - 1) and (ro_di[i] < ro_di[num_list[idx + 1]]):
-                add_list.append(ro_di[num_list[idx + 1]] - ro_di[i])
-                del num_list[idx]
-                if idx + 1 <= len(num_list) - 1:
+            elif (idx + 1 <= len(num) - 1) and (rom[i] < rom[num[idx + 1]]):
+                add.append(rom[num[idx + 1]] - rom[i])
+                del num[idx]
+                if idx + 1 <= len(num) - 1:
                     continue
                 else:
                     break
-            add_list.append(ro_di[i])
-        for i in add_list:
+            add.append(rom[i])
+        for i in add:
             result = result + i
         return(result)
     else:
