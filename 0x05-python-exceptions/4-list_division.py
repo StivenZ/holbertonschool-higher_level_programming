231631882b70b21e76eaeb3c42b42d52e855f189
stivenz <1806@holbertonschool.com>
2020-09-21 23:47:45 -0500

    Prints several imposible diivisons

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,11 +6,14 @@ def list_division(my_list_1, my_list_2, list_length):
             result = my_list_1[i] / my_list_2[i]
             new_list.append(result)
         except ZeroDivisionError:
+            new_list.append(0)
             print("division by 0")
         except TypeError:
+            new_list.append(0)
             print("wrong type")
         except IndexError:
+            new_list.append(0)
             print("out of range")
         finally:
-            new_list.append(0)
+            pass
     return (new_list)
