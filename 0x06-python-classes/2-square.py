f8e56dd637bb39357b8fdd9e849f3c2421fb65c2
stivenz <1806@holbertonschool.com>
2020-09-22 21:38:45 -0500

    Raises an exception

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,6 +7,6 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
         if not type(size) is int:
-            raise TypeError("size most be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
