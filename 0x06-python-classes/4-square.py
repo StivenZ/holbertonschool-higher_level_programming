a3aa7f4d28f5669e71ba029544468e18fd732ceb
stivenz <1806@holbertonschool.com>
2020-09-23 22:00:58 -0500

    Setter and gwetter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,21 +6,22 @@ class Square:
     """Square class with private instance"""
     def __init__(self, size=0):
         self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return (self.__size ** 2)
 
     @property
     def size(self):
-        """Getter method"""
-        return (self.size)
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """Setter method"""
-        if not type(value) is int:
+        if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    def area(self):
-        """Method to compute the square"""
-        return (self.__size ** 2)
