f38bf58dfa88d5fd92db93135e687033925c6e45
stivenz <1806@holbertonschool.com>
2020-09-26 17:12:15 -0500

    Adds integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
+"""Adds two integers
+"""
+
+
 def add_integer(a, b=98):
+    """Adds two integers
+
+    Args:
+        a ([type]): [description]
+        b (int, optional): [description]. Defaults to 98.
+
+    Raises:
+        TypeError: [description]
+        TypeError: [description]
+
+    Returns:
+        [type]: [description]
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     elif type(b) not in [int, float]:
