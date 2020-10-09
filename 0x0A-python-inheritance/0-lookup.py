6d9656a215643065e3ca2a7fcd5769630a7950c6
stivenz <1806@holbertonschool.com>
2020-10-08 19:19:36 -0500

    Prints object lists

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""Lists attributes and methods
+"""
+
 
 def lookup(obj):
+    """[summary]
+
+    Args:
+        obj ([class]): It's an instance of a class
+
+    Returns:
+        [list]: A list with attributes and methods of the currentoobject
+    """
     return (list(dir(obj)))
