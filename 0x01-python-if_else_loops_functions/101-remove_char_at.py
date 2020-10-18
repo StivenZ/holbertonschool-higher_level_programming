2e468ed5da1440f1d6bb085e8deb3a39a07fd040
stivenz <1806@holbertonschool.com>
2020-10-18 15:29:49 -0500

    Removes at index

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     new_str = list(str)
-    if n < len(str):
+    if n < len(str) and n >= 0:
         new_str.remove(str[n])
     return ("".join(new_str))
