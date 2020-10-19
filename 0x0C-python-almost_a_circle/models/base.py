a63ae247d788da9d57b7429a1aa3792796842eb3
stivenz <1806@holbertonschool.com>
2020-10-19 13:12:52 -0500

    Fixed style

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,7 +10,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
@@ -20,20 +20,34 @@ class Base:
     def to_json_string(list_dictionaries):
         """Turns json to string
         """
-        if list_dictionaries == None or list_dictionaries == {}:
-            return ("[]")
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
         else:
-            return (json.dumps(list_dictionaries))
+            return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
         """Saves json to a file
         """
-        if list_objs == None:
-            vary = "[]"
+        if list_objs is None:
+            vary = []
         else:
             vary = cls.to_json_string([i.to_dictionary() for i in list_objs])
         filename = cls.__name__ + ".json"
 
         with open(filename, mode="w", encoding="UTF-8") as _file:
             _file.write(vary)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None:
+            return ([])
+        else:
+            return (json.loads(json_string))
+
+    # @classmethod
+    # def create(cls, **dictionary):
+    #     if cls.__name__ == Square:
+    #         pass
+
+# new_instance = cls.__name__(1, 1)
