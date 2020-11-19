68e4a94b26f0bcbf188913d8fba8efdf92d92af1
stivenz <1806@holbertonschool.com>
2020-11-19 02:01:47 -0500

    Reference tables with foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,4 @@
+-- creates database with foreign key
+CREATE DATABASE hbtn_0d_usa IF NOT EXISTS;
+USE hbtn_0d_usa;
+CREATE TABLE cities IF NOT EXISTS (id INT AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL FOREIGN KEY REFERENCES hbtn_0d_usa.tables(id));
