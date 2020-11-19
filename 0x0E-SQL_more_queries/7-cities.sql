f2042457ff94acf845302bfea79879fba2669f1a
stivenz <1806@holbertonschool.com>
2020-11-19 02:07:32 -0500

    added city name

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,5 @@
 -- creates database with foreign key
 CREATE DATABASE hbtn_0d_usa IF NOT EXISTS;
 USE hbtn_0d_usa;
-CREATE TABLE cities IF NOT EXISTS (id INT AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL FOREIGN KEY REFERENCES hbtn_0d_usa.states(id));
+CREATE TABLE cities IF NOT EXISTS (id INT AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL FOREIGN KEY REFERENCES hbtn_0d_usa.states(id),
+    name VARCHAR(256) NOT NULL);
