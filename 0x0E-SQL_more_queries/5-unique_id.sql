ea59975322f55004140909d94086d19eec8ca0c7
stivenz <1806@holbertonschool.com>
2020-11-19 01:25:38 -0500

    unique id

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- creates table with default value and unique
-CREATE TABLE IF NOT EXISTS id_not_null (id INT NOT NULL DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT NOT NULL DEFAULT 1 UNIQUE, name VARCHAR(256));
