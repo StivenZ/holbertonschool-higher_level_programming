6864aa40c4d6a9f26ebaba2f95e7e0bc222ff9c8
stivenz <1806@holbertonschool.com>
2020-11-19 23:46:09 -0500

    Fixed

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- list cities of California
-SELECT id, name FROM cities ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id;
