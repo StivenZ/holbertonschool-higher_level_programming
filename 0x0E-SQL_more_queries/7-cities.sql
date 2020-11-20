a1077a5f2c39867ee15c7b2c6a340f1367ef4d5f
stivenz <1806@holbertonschool.com>
2020-11-19 23:30:01 -0500

    Fixed if not exists database

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- creates database with foreign key
-CREATE DATABASE hbtn_0d_usa IF NOT EXISTS;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS cities(
     id INT PRIMARY KEY AUTO_INCREMENT,
     state_id INT NOT NULL,
     FOREIGN KEY(state_id) REFERENCES states(id),
