aa9520fadc9c58b94118b52b60ffd71328c00afb
stivenz <1806@holbertonschool.com>
2020-11-19 23:21:00 -0500

    Syntaxys

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,8 @@
 -- creates database with foreign key
 CREATE DATABASE hbtn_0d_usa IF NOT EXISTS;
 USE hbtn_0d_usa;
-CREATE TABLE cities IF NOT EXISTS (id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE cities IF NOT EXISTS (id INT AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES states(id));
+    PRIMARY KEY (id),
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
