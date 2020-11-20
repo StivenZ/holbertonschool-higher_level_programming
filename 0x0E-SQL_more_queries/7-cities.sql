9d29a8cc04220cbb011ef7d7742f60dd1b24e434
stivenz <1806@holbertonschool.com>
2020-11-19 23:18:31 -0500

    Fixed syntaxis

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,5 +3,5 @@ CREATE DATABASE hbtn_0d_usa IF NOT EXISTS;
 USE hbtn_0d_usa;
 CREATE TABLE cities IF NOT EXISTS (id INT AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL
+    name VARCHAR(256) NOT NULL,
     FOREIGN KEY(state_id) REFERENCES states(id));
