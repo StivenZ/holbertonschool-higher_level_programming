25e808385cf1f4dfcb09f223ce5fc4a4c841e53e
stivenz <1806@holbertonschool.com>
2020-12-02 11:40:07 -0500

    Issue

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,2 @@
 -- list tv shows by genres ID
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres WHERE tv_shows.id = tv_show_genres.show_id AND tv_shows.id <= 1;
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres WHERE tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
