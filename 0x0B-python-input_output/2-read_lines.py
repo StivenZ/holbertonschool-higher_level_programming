f8c9a4b9a829f33d5733bac7784c8c1b8424f9d5
stivenz <1806@holbertonschool.com>
2020-12-02 21:54:11 -0500

    read n lines first

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Read n lines at first"""
+n = __import__('1-number_of_lines').number_of_lines
+
+
+def read_lines(filename="", nb_lines=0):
+    """Read lines"""
+
+    idx = 0
+
+    count = n(filename)
+
+    with open(filename, "r", encoding="utf-8") as fd:
+        if nb_lines >= count or nb_lines <= 0:
+            print(fd.read(), end="")
+        else:
+            for b in fd:
+                if idx < nb_lines:
+                    print(b, end="")
+                idx += 1
