55f88267e953d3faa222be04afb0eee0bd77e41d
stivenz <1806@holbertonschool.com>
2020-12-14 13:33:55 -0500

    Fixed querty

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    query = ("SELECT * FROM states WHERE states.name \
+    query = ("SELECT * FROM states WHERE name \
 LIKE BINARY '{}';".format(s_name))
     cur.execute(query)
     the_states = cur.fetchall()
