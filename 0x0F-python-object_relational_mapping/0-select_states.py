876a08be480364561596125bde4db40414a8b0ae
stivenz <1806@holbertonschool.com>
2020-12-14 00:19:03 -0500

    Fixed style with pep8

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,8 @@ if __name__ == '__main__':
     from sys import argv
 
     username, password, database = argv[1], argv[2], argv[3]
-    db = sql.connect(host="localhost", db=database, user=username, passwd=password, port=3306)
+    db = sql.connect(host="localhost", db=database, user=username,
+                     passwd=password, port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     for data in cur.fetchall():
