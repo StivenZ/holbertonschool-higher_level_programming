ed7295d625d6d39033c0f4f185c5bbc62e0b318a
stivenz <1806@holbertonschool.com>
2020-12-14 11:35:49 -0500

    Selects matching argument state

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,10 +11,9 @@ if __name__ == "__main__":
                      passwd=password, port=3306)
 
     cur = db.cursor()
-    query = ("SELECT * FROM states ORDER BY states.id ASC")
+    query = ("SELECT * FROM states WHERE states.name = '{}';".format(s_name))
     cur.execute(query)
     the_states = cur.fetchall()
 
     for name in the_states:
-        if name[1] == s_name:
-            print(name)
+        print(name)
