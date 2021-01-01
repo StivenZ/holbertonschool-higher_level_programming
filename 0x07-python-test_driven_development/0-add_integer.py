e3cab7936ee482823b58fa55df2e76bf6ddcb5a2
stivenz <1806@holbertonschool.com>
2020-12-31 19:37:56 -0500

    Fixed

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,7 +17,7 @@ def add_integer(a, b=98):
     Returns:
         [type]: [description]
     """
-    if type(a) not in [int, float]:
+    if a is None or (type(a) not in [int, float]):
         raise TypeError("a must be an integer")
     elif type(b) not in [int, float]:
         raise TypeError("b must be an integer")
