ff22644a0902484c16b3e380f0bfc530270b9c4b
stivenz <1806@holbertonschool.com>
2021-01-01 15:47:26 -0500

    Square and rectangle classes

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Empty class
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inherited from Rectangle
+    """
+    def __init__(self, size):
+        super().__init__(size, size)
+
+    def __str__(self):
+        return("[{}] {}/{}".format(self.__class__.__name__, self.width, self.height))
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Empty class
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inherited from Base geometry
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return("[{}] {}/{}".format(self.__class__.__name__, self.__width, self.__height))
+
+    def area(self):
+        return(self.__width * self.__height)
