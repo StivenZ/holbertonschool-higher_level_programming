20e73105c082de82d662df2fe5b89d83dd443cc8
stivenz <1806@holbertonschool.com>
2021-01-04 17:13:01 -0500

    Save and not append

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Save a json file into a file"""
-    with open(filename, "a", encoding="utf-8") as fd:
+    with open(filename, "w", encoding="utf-8") as fd:
         json.dump(my_obj, fd)
