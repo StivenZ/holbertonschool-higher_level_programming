e344857aeee88a76236c33f71e9e6a61c78a5635
stivenz <1806@holbertonschool.com>
2021-01-04 19:51:04 -0500

    Returns specified attributes

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module about JSON repr and usage with classes"""
+
+
+class Student():
+    """Define Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs:
+            new_dict = {}
+            for atr in attrs:
+                if atr in self.__dict__:
+                    new_dict[atr] = self.__dict__[atr]
+            return (new_dict)
+        else:
+            return (self.__dict__)
