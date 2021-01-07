8f5f276f5055b65c21fe6f9498c7514b055f7a02
stivenz <1806@holbertonschool.com>
2021-01-07 14:55:36 -0500

    Reload from json

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module about JSON repr and usage with classes"""
+
+
+class Student():
+    """Define Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            new_dict = {}
+            for atr in attrs:
+                if atr in self.__dict__:
+                    new_dict[atr] = self.__dict__[atr]
+            return (new_dict)
+        elif attrs is None:
+            return (self.__dict__)
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.key = value
