d3448e5ca3fae05518eb4e6842d812d2cd9f3a43
stivenz <1806@holbertonschool.com>
2021-01-13 14:19:02 -0500

    Fixed comments

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,7 +10,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialization
+        """Initialization of the class
         """
         if id is not None:
             self.id = id
