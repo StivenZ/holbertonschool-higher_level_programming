0869f134275f62133f4cd4d605933ba73315c671
stivenz <1806@holbertonschool.com>
2021-03-22 23:47:10 -0500

    Fixed styke

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,19 +4,18 @@ const tamaño = process.argv.length;
 
 if (tamaño === 2 && tamaño === 3) {
   console.log(0);
-  return;
-}
+} else {
+  let newArray = [];
 
-let newArray = [];
-
-for (const x of array) {
-  if (!isNaN(parseInt(x))) {
-    newArray.push(parseInt(x));
+  for (const x of array) {
+    if (!isNaN(parseInt(x))) {
+      newArray.push(parseInt(x));
+    }
   }
-}
 
-newArray = new Set(newArray);
-newArray = Array.from(newArray);
-newArray.sort((a, b) => a - b);
-newArray.pop();
-console.log(newArray[newArray.length - 1]);
+  newArray = new Set(newArray);
+  newArray = Array.from(newArray);
+  newArray.sort((a, b) => a - b);
+  newArray.pop();
+  console.log(newArray[newArray.length - 1]);
+}
