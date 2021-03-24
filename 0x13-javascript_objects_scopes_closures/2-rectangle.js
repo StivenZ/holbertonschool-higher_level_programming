9f440cffd7482861d0c2390f3af34188708b5de7
stivenz <1806@holbertonschool.com>
2021-03-24 00:14:17 -0500

    Rectangle conditions

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
-    if ((w || h) <= 0) {
-      
+    if ((h && w) && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
 }
