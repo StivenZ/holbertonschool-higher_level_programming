f278c7f2745587ed0f3c5c6706e6030fbc0839c8
stivenz <1806@holbertonschool.com>
2021-03-24 11:09:24 -0500

    Fixec method

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const SquareSuper = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends SquareSuper {
   constructor (size) {
     super(size, size);
   }
