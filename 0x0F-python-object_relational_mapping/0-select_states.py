3a1afb7b2778a59187169ae94e69f46c3fcadcb5
stivenz <1806@holbertonschool.com>
2021-03-27 13:01:56 -0500

    Import

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=psswd, db=db, port=3306)
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states ORDER BY state.id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     state = cur.fetchall()
     for states in state:
         print(states)
