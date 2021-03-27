869ab783a0aebb1a878392a8bf46036dcc0dcf11
stivenz <1806@holbertonschool.com>
2021-03-27 17:58:09 -0500

    Not executed when imported

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """Module on starting session with mysqlalchemy
 """
-import sqlalchemy
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+if __name__ == "__main__":
+    """List objects in ascending order
+    """
+    import sqlalchemy
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy.orm import sessionmaker
 
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:\
+                            3306/{}'.format(sys.argv[1], sys.argv[2],
+                                            sys.argv[3]), pool_pre_ping=True)
+    Base = declarative_base()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:\
-                        3306/{}'.format(sys.argv[1], sys.argv[2],
-                                        sys.argv[3]), pool_pre_ping=True)
-Base = declarative_base()
-Session = sessionmaker(bind=engine)
-session = Session()
-
-for dispatch in session.query(State).order_by(State.id.asc()):
-    print("{}: {}".format(dispatch.id, dispatch.name))
+    for dispatch in session.query(State).order_by(State.id.asc()):
+        print("{}: {}".format(dispatch.id, dispatch.name))
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 """Module on mysql alchemy filter
 """
-import sqlalchemy
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+if __name__ == "__main__":
+    """Display first object from query
+    """
+    import sqlalchemy
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy.orm import sessionmaker
 
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:\
+                            3306/{}'.format(sys.argv[1], sys.argv[2],
+                                            sys.argv[3]), pool_pre_ping=True)
+    Base = declarative_base()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:\
-                        3306/{}'.format(sys.argv[1], sys.argv[2],
-                                        sys.argv[3]), pool_pre_ping=True)
-Base = declarative_base()
-Session = sessionmaker(bind=engine)
-session = Session()
-
-my_query = session.query(State).first()
-if my_query is None:
-    print("Nothing")
-else:
-    print("{}: {}".format(my_query.id, my_query.name))
+    my_query = session.query(State).first()
+    if my_query is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(my_query.id, my_query.name))
