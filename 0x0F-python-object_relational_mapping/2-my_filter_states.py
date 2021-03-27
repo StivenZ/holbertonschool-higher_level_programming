e84cbbaf36eb0312b84d6b0cd49295f0b3335246
stivenz <1806@holbertonschool.com>
2021-03-27 15:21:12 -0500

    Fixed

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,7 +4,6 @@ if __name__ == "__main__":
     """Filter state on user input"""
     import MySQLdb
     import sys
-    import re
 
     uname, psswd = sys.argv[1], sys.argv[2]
     db, state_name = sys.argv[3], sys.argv[4]
