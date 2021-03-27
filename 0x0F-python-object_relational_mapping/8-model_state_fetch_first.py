e50b3e7afb8514edc79d98aa66079616a5263645
stivenz <1806@holbertonschool.com>
2021-03-27 11:45:28 -0500

    Retrieve first object from database

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sqlalchemy
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:\
+                        3306/{}'.format(sys.argv[1], sys.argv[2],
+                                        sys.argv[3]), pool_pre_ping=True)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+my_query = session.query(State).first()
+print("{}: {}".format(my_query.id, my_query.name))
