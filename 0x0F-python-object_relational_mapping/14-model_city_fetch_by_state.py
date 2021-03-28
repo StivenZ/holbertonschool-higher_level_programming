c7f45313124a37a07053910ec7cf341ebad39bef
stivenz <1806@holbertonschool.com>
2021-03-27 23:08:32 -0500

    Fetches states and cities id with names

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module on starting session with mysqlalchemy
+"""
+if __name__ == "__main__":
+    """Fetch objects from 'cities' table
+    """
+    import sqlalchemy
+    import sys
+    from model_state import Base, State
+    from model_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:\
+                            3306/{}'.format(sys.argv[1], sys.argv[2],
+                                            sys.argv[3]), pool_pre_ping=True)
+    Base = declarative_base()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for states, cities in session.query(State, City).order_by(City.id.asc()):
+        print("{}: ({}) {}".format(states.name, cities.id, cities.name))
