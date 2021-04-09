1c85bd3864c8632b65bf463791904fb8c7d57084
stivenz <1806@holbertonschool.com>
2021-04-09 18:10:55 -0500

    Fixed content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
 
     req = urllib.request.Request('https://intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
-        the_page = response.readlines()
+        the_page = response.read()
         print('Body response:')
-        for id in the_page:
-            print('\t- {}: {}'.format(id, type(id)))
+        print('\t- type: {}'.format(type(the_page)))
+        print('\t- content: {}'.format(the_page))
+        print('\t- utf8 content: {}'.format(the_page.decode('UTF-8')))
