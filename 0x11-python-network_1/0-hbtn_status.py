a21298f8d21f5fe21d11a5bacb7a1c8d71a003a7
stivenz <1806@holbertonschool.com>
2021-04-09 17:36:51 -0500

    Added decoding

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     req = urllib.request.Request('https://intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
+        the_page = the_page.decode('UTF-8')
         print('Body response:')
         for id in the_page:
             print('\t- {}: {}'.format(id, type(id)))
