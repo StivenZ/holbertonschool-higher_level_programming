10c564d0897706d2d9e3f778fdcde283b9d0f786
stivenz <1806@holbertonschool.com>
2021-04-09 21:58:26 -0500

    Fixed method

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
 
     r = requests.get('https://intranet.hbtn.io/status')
     print('Body response: ')
-    print('\t- type: ', type(r.content))
+    print('\t- type: ', type(r.text))
     print('\t- content: ', r.content.decode('UTF-8'))
