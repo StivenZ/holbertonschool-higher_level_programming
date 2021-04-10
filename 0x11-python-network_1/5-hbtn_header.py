ada2154a085406fea7b3f68d5cc76018d938d297
stivenz <1806@holbertonschool.com>
2021-04-09 21:56:31 -0500

    Prints variable value through requests package

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Fetching header"""
+
+if __name__ == "__main__":
+    """Prints a variable value with requests package
+    """
+    import requests
+    from sys import argv
+
+    r = requests.get(argv[1])
+    print(r.headers['X-Request-Id'])
