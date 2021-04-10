72285dca38fe3678782766f1ee1272153c0fe967
stivenz <1806@holbertonschool.com>
2021-04-09 22:03:12 -0500

    Fixed space

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     import requests
 
     r = requests.get('https://intranet.hbtn.io/status')
-    print('Body response: ')
+    print('Body response:')
     print('\t- type: ', type(r.text))
     print('\t- content: ', r.content.decode('UTF-8'))
