5cf283f69715838297d07445c6d8d48dfc9a2742
stivenz <1806@holbertonschool.com>
2021-05-04 19:34:46 -0500

    Completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+
+request(url, function (error, response, body, algo) {
+  if (error) {
+    console.log(error);
+  }
+  const myDict = JSON.parse(body);
+
+  const userDict = {};
+  let completed = 0;
+  let prevUser = 1;
+
+  for (const i of myDict) {
+    if (prevUser !== i.userId) {
+      prevUser = i.userId;
+      completed = 0;
+    }
+    if (i.completed) {
+      completed++;
+      userDict[i.userId] = completed;
+    }
+  }
+
+  console.log(userDict);
+});
