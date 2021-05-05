016ae2bea171dcfeed83bda0ef6c6fe970b4149c
stivenz <1806@holbertonschool.com>
2021-05-04 19:54:53 -0500

    Fixed deleting none tasks users

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -23,6 +23,11 @@ request(url, function (error, response, body, algo) {
       userDict[i.userId] = completed;
     }
   }
+  for (tasks of Object.keys(userDict)){
+      if (userDict[tasks] === 0) {
+          delete userDict.tasks;
+      }
+  }
 
   console.log(userDict);
 });
